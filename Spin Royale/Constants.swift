9000d64fc89c8d2a70c3c7111127a6460d4a3cc6
Mayank Jangid <226301131@gkv.ac.in>
2025-03-20 15:45:09 +0530

    Spin the wheel screen.... (Basic logic + Sound + Design)

---
+++ Spin Royale/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  EmojiSlotMachine
+//
+//  Created by Marcy Vernon on 5/30/20.
+//  Copyright © 2020 Marcy Vernon. All rights reserved.
+//
+
+import Foundation
+
+enum K {
+    static let emojiFont  = "Apple Color Emoji"
+    static let win        = "Winner!"
+    static let lose       = "3 In A Row"
+    static let sound      = "Slots"
+//    static let imageArray = ["🍋", "❤️", "🍒", "🍌", "🍊", "🔔", "🍉", "👑"]
+    static let imageArray = ["🍋", "❤️", "🍒"]
+    static let rattle     = "Rattle"
+}
